284cf342118b0b37a43e7d4a2bdc3491fd6f9de3
github@cybertekschool.com <github@cybertekschool.com>
2020-09-21 16:01:22 -0500

    added selen class

---
+++ src/SelenFacebk/verifyTitle.java
@@ -0,0 +1,16 @@
+package SelenFacebk;
+
+import org.openqa.selenium.WebDriver;
+
+public class verifyTitle{
+public static void verifyTitle(WebDriver driver,CharSequence searchTerm){
+	String actual=driver.getTitle();
+	if(actual.contains(searchTerm+"|Etsy")) {
+		System.out.println("Pass");
+	}else {
+		System.out.println("Fail");
+		System.out.println("Expected: "+searchTerm);
+		System.out.println("Actual: "+actual);
+	}
+}
+}
